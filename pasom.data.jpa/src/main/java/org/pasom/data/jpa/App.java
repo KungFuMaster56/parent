f40d9b10b40e129f4a6abc9bd18f82eff1e25dc6
dingdingdong <315299647@qq.com>
2015-05-07 17:24:35 +0800

    add

---
+++ pasom.data.jpa/src/main/java/org/pasom/data/jpa/App.java
@@ -0,0 +1,13 @@
+package org.pasom.data.jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

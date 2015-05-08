6cebd348c6a821d87dd11a4e5b93fc451ed3b885
dingdingdong <315299647@qq.com>
2015-05-08 20:54:33 +0800

    1

---
+++ pasom.data.svc/src/main/java/org/pasom/data/svc/App.java
@@ -0,0 +1,13 @@
+package org.pasom.data.svc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
